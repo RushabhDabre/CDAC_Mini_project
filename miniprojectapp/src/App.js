6a5d5515effdc20585f2aefa77c589dc3f6c3a62
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-12-19 10:20:24 +0530

    added a new Component and added changes in registration page

--- miniprojectapp/src/App.js
+++ miniprojectapp/src/App.js
@@ -1,25 +1,27 @@
 
-import {Link, Route,Routes} from 'react-router-dom';
-import Home from './Components/Home';
-import Login from './Components/Login';
+// import {Link, Route,Routes} from 'react-router-dom';
+// import Home from './Components/Home';
+// import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import Register from './Components/Register';
 
 function App() {
   return (
     <div>
-     
-     <nav>
+      <Navbar/>
+     <Register/>
+     {/* <nav>
         <Link to="/home">HOME</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
-
-     </nav>
+     </nav> */}
      
-     <h1>Appoint Booking Application</h1>
+     {/* <h1>Appoint Booking Application</h1> */}
      
-     <Routes>
+     {/* <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/Login" element={<Login/>} />
-    </Routes>
+    </Routes> */}
     </div>
   );
 }
--- miniprojectapp/src/Components/Register.js
+++ miniprojectapp/src/Components/Register.js
@@ -1,3 +1,26 @@
 export default function Register(){
-    
+    return (
+        <div className="container-fluid">
+        <div className="row shadow-lg p-4 m-4 d-flex" style={{"width": '45rem'}}>
+            <div className="col-6">
+                
+            </div>
+            <div className="col-6">
+                <div className="row g-3 align-items-center">
+                        <div className="col-auto">
+                            <label htmlFor="inputPassword6" className="col-form-label">Password</label>
+                        </div>
+                        <div className="col-auto">
+                            <input type="password" id="inputPassword6" className="form-control" aria-describedby="passwordHelpInline"/>
+                        </div>
+                        <div className="col-auto">
+                            <span id="passwordHelpInline" className="form-text">
+                                Must be 8-20 characters long.
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>  
+    )
 }
